4399f0fcdb90e9e0e1be1a1765b800214ff0ef13
Akihiro Tanaka <akihiro.fin@icloud.com>
2021-11-03 22:32:41 +0200

    luokan Varasto testien haarautumakaattavuus 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,6 +13,21 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_uudella_varastolla_virheellinen_tilavuus(self):
+        self.varasto = Varasto(-1)
+
+        self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
+    
+    def test_uudella_varastolla_virheellinen_saldo(self):
+        self.varasto = Varasto(10, -1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+    
+    def test_uudella_varastolla_enemman_saldoa_kuin_tilavuutta(self):
+        self.varasto = Varasto(10, 15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +53,20 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_lisaa_varastoon_virheellisella_syotteella(self):
+        self.assertIsNone(self.varasto.lisaa_varastoon(-1))
+
+    def test_lisaa_varastoon_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_varastosta_otetaan_virheellisen_maaran_juomaa(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0.0)
+
+    def test_ottaminen_palauttaa_oikean_maaran_ottaessa_enemman_kuin_jaljella(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(11), 0)
+
+    def test_tulostaa_luokan_varasto(self):
+        self.assertAlmostEqual(str(self.varasto), "saldo = 0, vielä tilaa 10" )
